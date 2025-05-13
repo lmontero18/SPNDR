0261981d1b93cdbd224f6a4e001b102af17f0e2e
lmontero18 <lmonterom879@gmail.com>
2025-05-12 22:00:52 -0600

    adding details

--- src/app/dashboard/plan/page.tsx
+++ src/app/dashboard/plan/page.tsx
@@ -1,3 +1,11 @@
 export default function PlanPage() {
-  return <div>Plan</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="text-3xl font-bold">Your Monthly Plan 📅</h1>
+      <p className="text-sm">
+        Review your monthly goals, manage your income and expenses, and keep
+        track of your savings—all in one place.
+      </p>
+    </div>
+  );
 }
